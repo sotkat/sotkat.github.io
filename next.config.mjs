72e470166578286a063ad9c3bb252f692c80b725
Xavier Mod <hi.xavier.mod@gmail.com>
2024-05-11 13:21:23 +0100

    Progress

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
