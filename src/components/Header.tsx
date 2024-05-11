e6fc7db8de9943e9d86f5d66fb85b2dbcfa6146f
Xavier Mod <hi.xavier.mod@gmail.com>
2024-05-11 17:21:58 +0100

    Add more meta

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,6 +7,7 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  overflow-x: hidden;
 `;
 
 const Block = styled.div`
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,41 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>
-          Soto Coffee London | Single-origin Colombian coffee in London
-        </title>
+        <meta
+          name="title"
+          content="Soto Coffee | Independent cafe serving Colombian coffee in London"
+        />
         <meta
           name="description"
-          content="Single-origin colombian coffee in london"
+          content="Single-origin Colombian coffee right at the heart of Caledonian Road in North London. Come and taste the most organic Colombian coffee!"
         />
+
+        {/** Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.sotocoffee.co.uk/" />
+        <meta
+          property="og:title"
+          content="Soto Coffee | Independent cafe serving Colombian coffee in London"
+        />
+        <meta
+          property="og:description"
+          content="Single-origin Colombian coffee right at the heart of Caledonian Road in North London. Come and taste the most organic Colombian coffee!"
+        />
+        <meta property="og:image" content="/images/meta-image.png" />
+
+        {/** Twitter */}
+        <meta property="twitter:card" content="/images/meta-image.png" />
+        <meta property="twitter:url" content="https://www.sotocoffee.co.uk/" />
+        <meta
+          property="twitter:title"
+          content="Soto Coffee | Independent cafe serving Colombian coffee in London"
+        />
+        <meta
+          property="twitter:description"
+          content="Single-origin Colombian coffee right at the heart of Caledonian Road in North London. Come and taste the most organic Colombian coffee!"
+        />
+        <meta property="twitter:image" content="/images/meta-image.png" />
+
         <meta
           name="keywords"
           content="colombian coffee, london, london coffee, single origin coffee, specialty coffee london, cafe, london cafe, colombian cafe, best coffee in london, best colombian coffee in london, soto colombian coffee"
